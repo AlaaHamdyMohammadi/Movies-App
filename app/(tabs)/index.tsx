0ecc97f23858f8cf11e87766033fef7471752b48
AlaaHamdy <alaahamdy2197@gmail.com>
2025-05-09 18:02:04 +0300

    feat: ✨ implement navigation structure

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,6 +8,7 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        gap: 20,
       }}
     >
       <Text className="font-bold text-2xl text-dark-200">Welcome Alaa ❤️</Text>
